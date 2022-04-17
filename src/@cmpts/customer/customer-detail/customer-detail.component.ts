a9ff5c4a35199f653ac87b27886337076441df93
sunhao <2080680175@@qq.com>
2022-04-17 19:54:24 +0800

    customer detail layout.

--- src/@cmpts/customer/customer-detail/customer-detail.component.ts
+++ src/@cmpts/customer/customer-detail/customer-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-customer-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      console.log(params['id']);
+    })
   }
 
 }
