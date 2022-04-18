2352bb0b4af8eb1e1d8af35f14534818ca924a0c
sunhao <2080680175@@qq.com>
2022-04-18 11:01:38 +0800

    customer info remark.

--- src/@cmpts/customer/account-info/account-info.component.ts
+++ src/@cmpts/customer/account-info/account-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogCustomerComponent } from '../dialog-customer/dialog-customer.component';
+import { CustomerElement } from '../model';
 
 @Component({
   selector: 'app-account-info',
@@ -13,9 +16,28 @@ export class AccountInfoComponent implements OnInit {
   loginedAt: Date = new Date();
   remark: string = '这是备注';
   avatar: string = '/assets/images/logo_256.png';
-  constructor() { }
+
+  customer: CustomerElement = {
+    avatar: '/assets/images/people_1.jpg', name: 'zhangsan', nickname: '张三',
+    constraint: '有', lastLoginAt: new Date(), remark: '这是假数据'
+  };
+
+  constructor(private dialog: MatDialog,) { }
 
   ngOnInit() {
   }
 
+  remark_click(): void {
+    const dialogRef = this.dialog.open(DialogCustomerComponent, {
+      width: '360px',
+      data: this.customer,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.op === 'save') {
+        this.customer.remark = result.remark;
+        this.remark = result.remark;
+      }
+    });
+  }
 }
